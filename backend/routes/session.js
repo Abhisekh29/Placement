9c9e4d76bf0ad8ef41fc8ae79a218f99ab8da543
Abhisekh29 <abhisekhroy2912@gmail.com>
2025-09-15 21:05:48 +0530

    Separated Student Detail Form and applied conditional rendering for it

--- backend/routes/session.js
+++ backend/routes/session.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { getSessions } from "../controllers/session.js";
 
 const router = express.Router();
+
+router.get("/", getSessions);
 
 export default router;
--- backend/routes/student.js
+++ backend/routes/student.js
@@ -1,5 +1,9 @@
 import express from "express";
+import { addStudent, getStudentDetails } from "../controllers/student.js";
 
 const router = express.Router();
+
+router.get("/:userid", getStudentDetails);
+router.post("/", addStudent)
 
 export default router;
